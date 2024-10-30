f89ac5d33380c097b45941f8053d08409bb07e22
AljoharahIbrahim <jalohydab@gmail.com>
2024-10-30 10:52:43 +0300

    fetch get products from backend

--- src/context/ProductContextData.jsx
+++ src/context/ProductContextData.jsx
@@ -8,36 +8,19 @@ export const ProductContext = createContext({});
 export default function ProductContextData({ children }) {
   // ctrate data state
   const [products, setProducts] = useState([]);
-  console.log("***----");
-  // implement fetchdata function that connect with api to get products data inside useeffect
-  useEffect(() => {
-    fetch(
-      "https://sda-3-onsite-backend-teamwork-py8b.onrender.com/api/v1/products"
-    )
-      .then((res) => res.json())
-      .then((json) => setProducts(json))
-      .catch((error) => console.error("Error:", error));
-  }, []);
-
   // use axios to function that connect with api to get products data inside useeffect
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //  await axios
-  //    .get(
-  //      "https://sda-3-onsite-backend-teamwork-py8b.onrender.com/api/v1/products"
-  //    )
-  //    .then((response) => {
-  //     console.log("----ProductContextData----");
-  //      console.log(response.data);
-  //      setProducts(response.data);
-  //    })
-  //    .catch((error) => {
-  //      console.log(error);
-  //    });
-  //    }
-  //   fetchData();
-  // }, []);
+  const getProducts = () => {
+    axios
+      .get(
+        "https://sda-3-onsite-backend-teamwork-py8b.onrender.com/api/v1/products"
+      )
+      .then((response) => {
+        // console.log("data: ", response.data.data.items.$values[0].name);
+        const productData = response.data.data.items.$values;
+        setProducts(productData);
+      });
+  };
+  useEffect(() => getProducts(), []);
 
   return (
     <div>
--- src/pages/ProductsListPage.jsx
+++ src/pages/ProductsListPage.jsx
@@ -1,21 +1,39 @@
-import React, { useContext, useState } from "react";
+import React, { useContext } from "react";
 import { ProductContext } from "../context/ProductContextData";
 
 export default function ProductsListPage() {
   // import product context
   const context = useContext(ProductContext);
-  console.log(" ***** from ProductsListPage");
-  console.log("context ", context.products);
+  // console.log("context ", context.products);
+
   return (
     <div>
-      <h1>ProductsListPage</h1>
-      {/* {Products.length > 0 ? (
-        Products.map((product) => <div key={product.id}>{product.name}</div>)
-      ) :
-        (
+      <h2>Products List Page</h2>
+      {context.products.length > 0 ? (
+        context.products.map((product) => (
+          <div key={product.id}>
+            <p>
+              <strong>Name:</strong>
+              {product.name}
+            </p>
+            <p>
+              <strong>Category:</strong>
+              {product.categoryName}
+            </p>
+            <p>
+              <strong>StockQuantity:</strong>
+              {product.stockQuantity}
+            </p>
+            <p>
+              <strong>updatedDate:</strong>
+              {product.updatedDate}
+            </p>
+            <br></br>
+          </div>
+        ))
+      ) : (
         <p>no prodect found </p>
-      )} */}
-      {/* {Products} */}
+      )}
     </div>
   );
 }
