23f2abb0590b072d11966ab2d38d0fc8ae5dfbf9
AljoharahIbrahim <jalohydab@gmail.com>
2024-10-31 15:23:53 +0300

    clean product service

--- src/services/productService.jsx
+++ src/services/productService.jsx
@@ -14,17 +14,4 @@ export const getAllProducts = async () => {
   return response.data;
 };
 
-export const getAllProductsByName = async (productName) => {
-  console.log(productName);
-  // extract the variable of url for pagenation & search & sort by
-  const search = window.location.search;
-  const params = new URLSearchParams(search);
-  // in case there are no assign value in variable => assign empty string tnstend of null value
-  let pageLimit = params.get("pageLimit") ?? "";
-  const pageNumber = params.get("pageNumber") ?? "";
-  const searchBy = productName;
-  const sort = params.get("sort") ?? "";
-  const url = `https://sda-3-onsite-backend-teamwork-py8b.onrender.com/api/v1/products?pageLimit=${pageLimit}&pageNumber=${pageNumber}&searchBy=${searchBy}&sort=${sort}`;
-  const response = await axios.get(url);
-  return response.data;
-};
+
