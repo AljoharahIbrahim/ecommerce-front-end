d8766a462da667eb52f2da95243049f8d4776ad2
AljoharahIbrahim <jalohydab@gmail.com>
2024-11-05 16:27:39 +0300

    add jwtDecode

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -3,8 +3,14 @@ import React, { useContext, useEffect } from "react";
 import Styles from "../styles/Login.module.css";
 import { UserContext } from "../context/UserContextData";
 import Error from "../pages/Error";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+
+
 
 export default function LoginForm() {
+const navigate = useNavigate();
+
   const {
     userEmail,
     setUserEmail,
@@ -36,11 +42,19 @@ export default function LoginForm() {
 
   // 3. use useeffect to accses usertoken after updates
   useEffect(() => {
-    //4. in case return tokens => set in local storage
-    //5. go to profile user " navigatee and send user data in state"
-    if (userData) {
-      console.log("userData=** ", userData);
-    }
+      if (userData) {
+        //4. in case return tokens => set in local storage
+        console.log("userData=** ", userData);
+        localStorage.setItem(
+          "login",
+          JSON.stringify({ userToken: userData, isSignIn: true })
+        );
+        console.log("success");
+        const token = userData;
+        const decoded = jwtDecode(token);
+        console.log(decoded);
+        //5. call getUserByID => go to profile user " navigatee and send user data in state"
+      }
   }, [userToken, userData]);
 
   return (
