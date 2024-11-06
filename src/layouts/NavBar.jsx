38fb2b968ca4e91613ce424bb710b55c82548ae0
AljoharahIbrahim <jalohydab@gmail.com>
2024-11-06 14:18:56 +0300

    add logout in navbar in case the user login show

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 import Styles from "../styles/NavBar.module.css";
@@ -8,12 +8,17 @@ export default function NavBar() {
   const [isDisabled, setIsDisabled] = useState(false);
   const { setLoginData, setUserEmail, setUserPassword } =
     useContext(UserContext);
-  // in case local storage not empty show the logout button
-  if (localStorage.removeItem("login") != null) {
-    setIsDisabled((prev) => !prev);
-  }
 
-  //
+  // in case local storage not empty show the logout button
+  useEffect(() => {
+    const checkStorage = localStorage.getItem("login");
+    if (checkStorage === null) {
+      setIsDisabled(true);
+    } else {
+      setIsDisabled(false);
+    }
+  }, []);
+
   const navigate = useNavigate();
   const logout = () => {
     localStorage.removeItem("login");
@@ -21,7 +26,7 @@ export default function NavBar() {
     setUserEmail();
     setUserPassword();
     // hide logoutbutton after delete storgae
-    setIsDisabled((prev) => !prev);
+    setIsDisabled(true);
     navigate("/");
   };
   return (
